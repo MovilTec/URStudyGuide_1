657a01df4a0a5392cc604d0315ef6522e3d52512
DaNiELChIoRo <da.leon1917@gmail.com>
2020-04-25 23:28:59 -0500

    chore: Implementing the back button action

--- app/src/main/java/com/example/urstudyguide_migration/Quizzes/QuizzAttempts.java
+++ app/src/main/java/com/example/urstudyguide_migration/Quizzes/QuizzAttempts.java
@@ -3,6 +3,7 @@ package com.example.urstudyguide_migration.Quizzes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.urstudyguide_migration.Quizzes.ui.quizzattempts.QuizzAttemptsFragment;
 import com.example.urstudyguide_migration.R;
@@ -18,5 +19,17 @@ public class QuizzAttempts extends AppCompatActivity {
                     .replace(R.id.container, QuizzAttemptsFragment.newInstance())
                     .commitNow();
         }
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                super.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
